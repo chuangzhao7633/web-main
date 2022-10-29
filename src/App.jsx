ea432de4650ab037c3728f46436022b2c60b4c00
chuangzhao <chuang.zhao666@foxmail.com>
2022-10-29 22:44:23 +0800

    微前端-主项目-开发环境脚手架基本配置

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React, { Suspense, lazy } from 'react';
+import { Link, Routes, Route } from "react-router-dom";
+/* import Home from './pages/Home';
+import About from './pages/About'; */
+
+const Home = lazy(() => import(/* webpackChunkName: 'home' */'./pages/Home'));
+const About = lazy(() => import(/* webpackChunkName: 'about' */'./pages/About'));
+
+export default function App() {
+  return (
+    <>
+      <div>App</div>
+      <ul>
+        <li>
+          <Link to='/home'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
+      <Suspense fallback={<div>loading...</div>}>
+        <Routes>
+          <Route path='/home' element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </Suspense>
+    </>
+  )
+}
