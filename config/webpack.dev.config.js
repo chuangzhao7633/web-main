3a4e2a38ffa6ed9c968a8b8dc9e695a7bef393ca
chuangzhao <chuang.zhao666@foxmail.com>
2022-10-30 21:14:00 +0800

    微前端-主项目-react脚手架优化配置

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -1,97 +1,9 @@
-const os = require('os');
-const path = require('path');
-const EslintWebpackPlugin = require('eslint-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { merge } = require('webpack-merge');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const base = require('./webpack.base.config');
 
-const threads = os.cpus().length; // cpu 核心数
-
-const getStyleLoaders = () => [
-  'style-loader',
-  'css-loader',
-  {
-    /* 
-      处理 css 兼容性问题
-      配合 package.json 中的 browserslist 指定兼容性
-    */
-    loader: 'postcss-loader',
-    options: {
-      postcssOptions: {
-        plugins: ['postcss-preset-env']
-      }
-    }
-  }
-];
-
-module.exports = {
-  entry: './src/main.js',
-  output: {
-    path: undefined,
-    filename:'static/js/[name].js',
-    chunkFilename: 'static/js/[name].chunk.js',
-    assetModuleFilename: 'static/images/[name].[hash:8][ext][query]'
-  },
-  module: {
-    rules: [
-      // 处理 css
-      {
-        test: /\.css$/,
-        use: getStyleLoaders()
-      },
-      // 处理 less
-      {
-        test: /\.less$/,
-        use:[...getStyleLoaders(), 'less-loader']
-      },
-      // 处理图片
-      {
-        test: /\.(jpe?g|png|gif|webp|svg)/,
-        type: 'asset',
-        parser: {
-          dataUrlCondition: {
-            maxSize: 10 * 1024
-          }
-        }
-      },
-      // 处理其他资源
-      {
-        test: /\.(woff2?|ttf)/,
-        type: 'asset/resource'
-      },
-      // 处理 js
-      {
-        test: /\.jsx?$/,
-        include: path.resolve(__dirname, '../src'),
-        use: [
-          {
-            loader: 'thread-loader', // 开启多进程对 babel 进行处理
-            options: {
-              works: threads // 进程数量
-            }
-          },
-          {
-            loader: 'babel-loader',
-            options: {
-              cacheDirectory: true,
-              cacheCompression: false,
-              plugins: ['react-refresh/babel'] // 激活 js 的 HMR 功能
-            }
-          }
-        ]
-      }
-    ]
-  },
+module.exports = () => merge(base(false), {
   plugins: [
-    new EslintWebpackPlugin({
-      context: path.resolve(__dirname, '../src'),
-      exclude: "node_modules",
-      cache: true,
-      cacheLocation: path.resolve(__dirname, '../node_modules/.cache/.eslintcache'),
-      threads // 多进程打包
-    }),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '../public/index.html')
-    }),
     new ReactRefreshWebpackPlugin() // 激活 js 的 HMR 功能
   ],
   mode: 'development',
@@ -99,15 +11,7 @@ module.exports = {
   optimization: {
     splitChunks: {
       chunks: 'all'
-    },
-    runtimeChunk: {
-      name: entrypoint => `runtime~${entrypoint.name}.js`
     }
-  },
-  // webpack 解析模块加载选项
-  resolve: {
-    // 自动补全文件扩展名
-    extensions: ['.jsx', '.js', '.json']
   },
   devServer: {
     host: 'localhost',
@@ -116,4 +20,4 @@ module.exports = {
     hot: true,
     historyApiFallback: true // 解决前端路由刷新 404 问题
   }
-}
+})
--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,101 +1,16 @@
 const os = require('os');
 const path = require('path');
-const EslintWebpackPlugin = require('eslint-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { merge } = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
 const TerserWebpackPlugin = require('terser-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const base = require('./webpack.base.config');
 
 const threads = os.cpus().length; // cpu 核心数
 
-const getStyleLoaders = () => [
-  // 提取 css 成单独文件
-  MiniCssExtractPlugin.loader,
-  'css-loader',
-  {
-    /* 
-      处理 css 兼容性问题
-      配合 package.json 中的 browserslist 指定兼容性
-    */
-    loader: 'postcss-loader',
-    options: {
-      postcssOptions: {
-        plugins: ['postcss-preset-env']
-      }
-    }
-  }
-];
-
-module.exports = {
-  entry: './src/main.js',
-  output: {
-    path: path.resolve(__dirname, '../dist'),
-    filename:'static/js/[name].[contenthash:8].js',
-    chunkFilename: 'static/js/[name].[contenthash:8].chunk.js',
-    assetModuleFilename: 'static/images/[name].[hash:8][ext][query]',
-    clean: true
-  },
-  module: {
-    rules: [
-      // 处理 css
-      {
-        test: /\.css$/,
-        use: getStyleLoaders()
-      },
-      // 处理 less
-      {
-        test: /\.less$/,
-        use:[...getStyleLoaders(), 'less-loader']
-      },
-      // 处理图片
-      {
-        test: /\.(jpe?g|png|gif|webp|svg)/,
-        type: 'asset',
-        parser: {
-          dataUrlCondition: {
-            maxSize: 10 * 1024
-          }
-        }
-      },
-      // 处理其他资源
-      {
-        test: /\.(woff2?|ttf)/,
-        type: 'asset/resource'
-      },
-      // 处理 js
-      {
-        test: /\.jsx?$/,
-        include: path.resolve(__dirname, '../src'),
-        use: [
-          {
-            loader: 'thread-loader', // 开启多进程对 babel 进行处理
-            options: {
-              works: threads // 进程数量
-            }
-          },
-          {
-            loader: 'babel-loader',
-            options: {
-              cacheDirectory: true,
-              cacheCompression: false
-            }
-          }
-        ]
-      }
-    ]
-  },
+module.exports = () => merge(base(true), {
   plugins: [
-    new EslintWebpackPlugin({
-      context: path.resolve(__dirname, '../src'),
-      exclude: "node_modules",
-      cache: true,
-      cacheLocation: path.resolve(__dirname, '../node_modules/.cache/.eslintcache'),
-      threads // 多进程打包
-    }),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '../public/index.html')
-    }),
     new MiniCssExtractPlugin({
       filename: 'static/css/[name].[contenthash:8].css',
       chunkFilename: 'static/css/[name].[contenthash:8].chunk.css'
@@ -104,12 +19,8 @@ module.exports = {
       patterns: [
         // 打包时将 public 下的文件复制到 dist 文件下
         { 
-          from: path.resolve(__dirname, '../public'),
-          to: path.resolve(__dirname, '../dist'),
-          globOptions: {
-            // 忽略 index.html 文件
-            ignore: ["**/index.html"]
-          }
+          from: path.resolve(__dirname, '../static'),
+          to: path.resolve(__dirname, '../dist/static')
         }
       ]
     })
@@ -135,17 +46,11 @@ module.exports = {
         }
       }
     },
-    runtimeChunk: {
-      name: entrypoint => `runtime~${entrypoint.name}.js`
-    },
     minimizer:[
       new CssMinimizerWebpackPlugin(),
-      new TerserWebpackPlugin()
+      new TerserWebpackPlugin({
+        parallel: threads // 开启多进程和进程数量处理 Terser
+      })
     ]
-  },
-  // webpack 解析模块加载选项
-  resolve: {
-    // 自动补全文件扩展名
-    extensions: ['.jsx', '.js', '.json']
   }
-}
+});
